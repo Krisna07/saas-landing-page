7c287903e022857d8318535993c5c9246fc06db3
Krishna <krisnachhetri07@gmail.com>
2023-03-27 13:09:15 +1100

    hero

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
+  const tube = <div className="tube" style={{}}></div>;
   return (
     <div className="App">
       <Navbar />
+      <div className="hero">{tube}</div>
     </div>
   );
 }
