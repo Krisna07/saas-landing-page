225195ba5a23d2db940c6fdb3c888fb55a8579d5
Krishna <krisnachhetri07@gmail.com>
2023-04-05 23:59:19 +1000

    testimonials

---
+++ src/Components/Sections/Testimonila.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Testimonila = () => {
+  const testimonials = [
+    {
+      stars: 4,
+      comment:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat, facilis!",
+      customerName: "Brroklyn simmons",
+    },
+  ];
+  return <div>lorem10</div>;
+};
+
+export default Testimonila;
