64b53e813146b02493940619db3f6276427ae719
Krishna Paudel <krisnachhetri07@gmail.com>
2023-07-09 13:28:21 +1000

    images added

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -58,22 +58,10 @@ const Hero = () => {
       <div className="laptopMockupContainer">
         <div className="laptopContainer">
           <div className="laptopMockup">
-            <img
-              src="https://s3-alpha-sig.figma.com/img/590d/59e7/46e6e2148986c4d0850c4434393b07c0?Expires=1681084800&Signature=pGe8cB8TnwZ5bIzFgWwzsguDUsYuzZyUOnidQU3NmR2CYu~rZBlfQ2kbYJepCu7-4POfP8~2-aGnFLK3olh92M6LceUsZMaJqla1Pw6tedEZKPDyKHFiy1TwNCQzQVj7OXsWTxjP9r8RC0ZyeruRgKxQfpYMye27vOWCNxrViin5yl8LUTl8tOk7z61t6VL-1262~sJiWtPiah1vOlXczESvlsO3WO4hZ~W4WAc8VLNze0d8GUSlSCmf2X2YcUgkqfd-Llc0vVpKwjezFOvavvgMWBj8hJTOykIvSiyB287uI5IjXXHEveeKxB~4LlERDdZm1X8MnJlJuMwFpd1~BQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-              width={"1000px"}
-              alt=""
-            />
-            <img
-              className="camera"
-              src="https://s3-alpha-sig.figma.com/img/2a87/89ed/b56d282b5aa545a970dfbe5ff401e172?Expires=1681084800&Signature=fkl3gFaObngac7D6m2tGJ~hode-P6yPWa1AmF5fUs23FKD0~IOAYIQ1d9spmRdlNCAIiG5h3m~G8aUonNStc-W5XnKVy~TmEmIhDLuJ3pqfBUHOhX~b1kblM0hgQCCtaR17Z~F8FId2sGXIDulcCoQXznx-ciXnRh5dGKXtwXuu3kIuPxcEhXcB7y19JRUdc1KEJn9FC6M-n9PfPlZqVUU288KSFBH0uFmsX1cDgKUz1CGQtQK8SklLPIwbGVkQgtmk5IPl9y8krH7fMO7pRCAfP7XzfaZ20Hw-6A9WwOgEZP~7Sa9TUbm~WHbzdPKG1yEfcK~-yjY3kc6LqzltadA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-              alt=""
-            />
+           <img src="/Images/laptopmockup.png" alt="" className="laptopImage"  />
+            
             <div className="analytics">
-              <img
-                className="analytics_image"
-                src="https://s3-alpha-sig.figma.com/img/3a82/b254/7ad7b570982e89338bc2c12e7e3b3b7c?Expires=1681084800&Signature=BcBNABO4TWXkO~NqKHrZJeXB7YenUZi0yFD0ND-~2MeDZy8CabTdvHfDAPy6F6CYpfYioHehS3g7WAQIOT0CgLcPqw2aZTUxQc8PXZDBK4zy3EFAplwGbFnY-E4~ektg-eXihrgqEla~av3OqSrHXNu-lUcF1JhGBEZq0PScUlJQavD1RU2VDzG-653~cD1slx3O1it04Pfs4CdY7bHgnj~aGVOfeKxlKRmEzcIAKfR0gjn8PmGLmxYNzDqubCHOHuOfqtHWU0D7Hwi0GKtk7FMgJlg2pdrLOYaxLw0vkUBytRMBqVjgvFLri18C05ihR6Lr1ED9Urf~E5ZI4-vrHQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-                alt=""
-              />
+           <img className="analytics_image" src="/Images/analytics.jpg" alt=""/>
             </div>
             <span className="laptopName">Macbook Pro</span>
           </div>
@@ -99,11 +87,8 @@ const Hero = () => {
                 </div>
                 <div className="iphoneCamera">
                   <div className="Frontcamera">
-                    <img
-                      className="camera"
-                      src="https://s3-alpha-sig.figma.com/img/2a87/89ed/b56d282b5aa545a970dfbe5ff401e172?Expires=1681084800&Signature=fkl3gFaObngac7D6m2tGJ~hode-P6yPWa1AmF5fUs23FKD0~IOAYIQ1d9spmRdlNCAIiG5h3m~G8aUonNStc-W5XnKVy~TmEmIhDLuJ3pqfBUHOhX~b1kblM0hgQCCtaR17Z~F8FId2sGXIDulcCoQXznx-ciXnRh5dGKXtwXuu3kIuPxcEhXcB7y19JRUdc1KEJn9FC6M-n9PfPlZqVUU288KSFBH0uFmsX1cDgKUz1CGQtQK8SklLPIwbGVkQgtmk5IPl9y8krH7fMO7pRCAfP7XzfaZ20Hw-6A9WwOgEZP~7Sa9TUbm~WHbzdPKG1yEfcK~-yjY3kc6LqzltadA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-                      alt=""
-                    />
+                  <div className="camera"></div>
+
                   </div>
                 </div>
                 <div className="timeContainer">
@@ -114,11 +99,8 @@ const Hero = () => {
               </div>
 
               <div className="analytics">
-                <img
-                  className="analytics_image"
-                  src="https://s3-alpha-sig.figma.com/img/3a82/b254/7ad7b570982e89338bc2c12e7e3b3b7c?Expires=1681084800&Signature=BcBNABO4TWXkO~NqKHrZJeXB7YenUZi0yFD0ND-~2MeDZy8CabTdvHfDAPy6F6CYpfYioHehS3g7WAQIOT0CgLcPqw2aZTUxQc8PXZDBK4zy3EFAplwGbFnY-E4~ektg-eXihrgqEla~av3OqSrHXNu-lUcF1JhGBEZq0PScUlJQavD1RU2VDzG-653~cD1slx3O1it04Pfs4CdY7bHgnj~aGVOfeKxlKRmEzcIAKfR0gjn8PmGLmxYNzDqubCHOHuOfqtHWU0D7Hwi0GKtk7FMgJlg2pdrLOYaxLw0vkUBytRMBqVjgvFLri18C05ihR6Lr1ED9Urf~E5ZI4-vrHQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-                  alt=""
-                />
+              <img className="analytics_image" src="/Images/analytics.jpg" alt=""/>
+
               </div>
               <div className="iphoneBottom"></div>
             </div>
@@ -129,18 +111,11 @@ const Hero = () => {
           <div className="tabContainer">
             <div className="tabMockup">
               <div className="Frontcamera">
-                <img
-                  className="camera"
-                  src="https://s3-alpha-sig.figma.com/img/2a87/89ed/b56d282b5aa545a970dfbe5ff401e172?Expires=1681084800&Signature=fkl3gFaObngac7D6m2tGJ~hode-P6yPWa1AmF5fUs23FKD0~IOAYIQ1d9spmRdlNCAIiG5h3m~G8aUonNStc-W5XnKVy~TmEmIhDLuJ3pqfBUHOhX~b1kblM0hgQCCtaR17Z~F8FId2sGXIDulcCoQXznx-ciXnRh5dGKXtwXuu3kIuPxcEhXcB7y19JRUdc1KEJn9FC6M-n9PfPlZqVUU288KSFBH0uFmsX1cDgKUz1CGQtQK8SklLPIwbGVkQgtmk5IPl9y8krH7fMO7pRCAfP7XzfaZ20Hw-6A9WwOgEZP~7Sa9TUbm~WHbzdPKG1yEfcK~-yjY3kc6LqzltadA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-                  alt=""
-                />
+              <div className="camera"></div>
               </div>
               <div className="analytics">
-                <img
-                  className="analytics_image"
-                  src="https://s3-alpha-sig.figma.com/img/3a82/b254/7ad7b570982e89338bc2c12e7e3b3b7c?Expires=1681084800&Signature=BcBNABO4TWXkO~NqKHrZJeXB7YenUZi0yFD0ND-~2MeDZy8CabTdvHfDAPy6F6CYpfYioHehS3g7WAQIOT0CgLcPqw2aZTUxQc8PXZDBK4zy3EFAplwGbFnY-E4~ektg-eXihrgqEla~av3OqSrHXNu-lUcF1JhGBEZq0PScUlJQavD1RU2VDzG-653~cD1slx3O1it04Pfs4CdY7bHgnj~aGVOfeKxlKRmEzcIAKfR0gjn8PmGLmxYNzDqubCHOHuOfqtHWU0D7Hwi0GKtk7FMgJlg2pdrLOYaxLw0vkUBytRMBqVjgvFLri18C05ihR6Lr1ED9Urf~E5ZI4-vrHQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-                  alt=""
-                />
+              <img className="analytics_image" src="/Images/analytics.jpg" alt=""/>
+
               </div>
               <div className="iphoneBottom"></div>
             </div>
